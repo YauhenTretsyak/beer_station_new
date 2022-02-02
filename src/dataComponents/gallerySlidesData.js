71917fcf5d57f645ae4c6882b34b76ff51c35bf8
Yauhen <zhendos@tut.by>
2022-02-02 14:43:04 +0100

    modified gallery's slider

--- src/dataComponents/gallerySlidesData.js
+++ src/dataComponents/gallerySlidesData.js
@@ -5,14 +5,19 @@ import lw_slide_3 from '../assets/slides/lwowska_3.jpg';
 // import lw_slide_5 from '../assets/slides/lwowska_5.jpg';
 // import lw_slide_6 from '../assets/slides/lwowska_6.jpg';
 
+import kn_slide_1 from '../assets/slides/kepna_1.jpg';
+import kn_slide_2 from '../assets/slides/kepna_2.jpg';
+import kn_slide_3 from '../assets/slides/kepna_3.jpg';
+import kn_slide_4 from '../assets/slides/kepna_4.jpg';
+import kn_slide_5 from '../assets/slides/kepna_5.jpg';
+
 const gallerySlidesData = {
   kepna: [
-    { slide: lw_slide_1 },
-    { slide: lw_slide_2 },
-    { slide: lw_slide_3 },
-    // { slide: lw_slide_4 },
-    // { slide: lw_slide_5 },
-    // { slide: lw_slide_6 },
+    { slide: kn_slide_1 },
+    { slide: kn_slide_2 },
+    { slide: kn_slide_3 },
+    { slide: kn_slide_4 },
+    { slide: kn_slide_5 },
   ],
 
   lwowska: [
