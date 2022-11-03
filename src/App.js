0331bf8e6d5a28cb7c0298fda38fe4ff4af141ca
Evgen Tretsyak <evgentretsyak@MacBook-Pro-Yauhen.local>
2022-11-03 15:36:17 +0100

    added homepage to package.json

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 /* eslint-disable react/react-in-jsx-scope */
 import {HelmetProvider} from 'react-helmet-async'
 import {ThemeProvider} from 'styled-components'
-import SwitchContextProvider from './context/SwitchContext'
 import GlobalStyle from './styles/GlobalStyle'
-import {Router, Routes, Route} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
 import {theme} from './styles/theme'
 import {HelmetBlock} from './common-components/index'
 import {MainPage, BeerCardPage} from './pages'
@@ -15,24 +14,22 @@ function App() {
     return (
         <HelmetProvider>
             <ThemeProvider theme={ theme } > 
-                <SwitchContextProvider>
-                    <GlobalStyle /> 
-                    <HelmetBlock
-                        title="BeerStation"
-                        name="BeerStation"
-                        content="beer"
-                        description="beerstation"
-                        faviconUrl={ favicon }
-                    />
-                    <Routes>
-                        <Route path="/" element={ <MainPage /> } />
-                        <Route path="/beer_page" element={ <BeerCardPage /> }>
-                            <Route path=":location/:id" element={ <BeerCardPage /> } />
-                        </Route>
-                        <Route path="/admin" element={ <AdminPanelMainPage /> } />
-                    </Routes>
-                    <Footer />
-                </SwitchContextProvider>
+                <GlobalStyle /> 
+                <HelmetBlock
+                    title="BeerStation"
+                    name="BeerStation"
+                    content="beer"
+                    description="beerstation"
+                    faviconUrl={ favicon }
+                />
+                <Routes>
+                    <Route path="/" element={ <MainPage /> } />
+                    <Route path="/beer_page" element={ <BeerCardPage /> }>
+                        <Route path=":location/:id" element={ <BeerCardPage /> } />
+                    </Route>
+                    <Route path="/admin" element={ <AdminPanelMainPage /> } />
+                </Routes>
+                <Footer />
             </ThemeProvider>
         </HelmetProvider>
     )
